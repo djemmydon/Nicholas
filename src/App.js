bbcb4c8c006dd2356979aa8572580f0c2be1c080
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2023-06-12 21:15:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+   
       <div className="www">
         <p>CarPro</p>
         <h2>We are currenctly Working On the website</h2>
       </div>
-    </>
+
     )
 }
 
