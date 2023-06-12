b421a9139265ca14e7f614055b425eb929c00eab
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2023-06-12 20:35:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <>
-  <Navbar />
-
-
-  <Hero textHeader="WELCOME TO PIXOVA LITE" textBig="Free & Modern One-Page Parallax WordPress Theme " textPara="Your cool business headline here. You can even insert HTML here & images. Lorem ipsum dolor sit amet lorem dolor sit amet."/>
-  
-  </>;
+  return (
+    <h2>We are still building</h2>
 }
 
 export default App;
